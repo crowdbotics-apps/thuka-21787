8b70f175365a147d9dbb72c755b55ba7b0fd126b
Crowdbotics <team@crowdbotics.com>
2020-10-21 12:22:28 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings153190', human_name: 'Settings', access_route: 'Settings153190'},
 { name: 'Settings153175', human_name: 'Settings', access_route: 'Settings153175'},
 { name: 'NotificationList153174', human_name: 'Notification List', access_route: 'NotificationList153174'},
 { name: 'Maps153173', human_name: 'Maps', access_route: 'Maps153173', icon: 'map'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings153190Navigator from '../features/Settings153190/navigator';
 import Settings153175Navigator from '../features/Settings153175/navigator';
 import NotificationList153174Navigator from '../features/NotificationList153174/navigator';
 import Maps153173Navigator from '../features/Maps153173/navigator';
@@ -16,6 +17,7 @@ import Maps153173Navigator from '../features/Maps153173/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Settings153190: { screen: Settings153190Navigator },
 Settings153175: { screen: Settings153175Navigator },
 NotificationList153174: { screen: NotificationList153174Navigator },
 Maps153173: { screen: Maps153173Navigator },
